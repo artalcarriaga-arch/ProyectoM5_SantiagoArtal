c37b594ad360b504592c274750ad6f4fba132c48
artalcarriaga-arch <artalcarriaga@gmail.com>
2026-06-22 17:44:29 -0300

    chore: anidar CartProvider en el punto de entrada principal main

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './context/AuthContext.tsx';
+import { CartProvider } from './context/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </AuthProvider>
   </React.StrictMode>,
 );
